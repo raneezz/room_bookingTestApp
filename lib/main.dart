82f746592ad566613c765d98173fc64cf9c4fe0a
Ranees <mail2ranees@gmail.com>
2026-09-14 18:51:31 +0530

    first_commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_instance/src/bindings_interface.dart';
+import 'package:get/get_instance/src/extension_instance.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:roombookingapp/screens/booking/controllers/BookingController.dart';
+import 'package:roombookingapp/screens/booking/pages/BookingPage.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,5 +16,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) =>
-      GetMaterialApp(debugShowCheckedModeBanner: false, title: 'Booking App');
+
+      GetMaterialApp(debugShowCheckedModeBanner: false,
+        title: 'Booking App',
+        initialBinding: BindingsBuilder(() => Get.put(BookingController())),
+        home: const BookingPage(),);
 }
